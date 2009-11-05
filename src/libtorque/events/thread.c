20ead2559d41e03ee567713f0a4139b85c27a21d
Nick Black <dank@qemfd.net>
2009-11-05 06:25:27 -0500

    add a comment about pthread cancellation, and set it up so that we're not cancellable during event handling. we'd like to encode cancellation into event handling itself so that we don't have to do this each time through

--- src/libtorque/events/thread.c
+++ src/libtorque/events/thread.c
@@ -1,4 +1,15 @@
+#include <pthread.h>
 #include <libtorque/events/thread.h>
 
+// We're currently cancelled. That isn't generally safe unless we wrap handling
+// with cancellation blocking, which eats a bit of performance. We'd like to
+// encode cancellation into event handling itself FIXME.
 void event_thread(void){
+	int oldcstate,newcstate = PTHREAD_CANCEL_DISABLE;
+
+	while(1){
+		pthread_setcancelstate(newcstate,&oldcstate);
+		pthread_setcancelstate(oldcstate,&newcstate);
+		pthread_testcancel();
+	}
 }
