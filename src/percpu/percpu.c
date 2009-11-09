a87b1a2e298eda480856c3e926b74c2426056475
Nick Black <dank@qemfd.net>
2009-11-09 13:34:52 -0500

    spawn and reap threads in percpu

--- src/percpu/percpu.c
+++ src/percpu/percpu.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <libtorque/libtorque.h>
 
 static void
 usage(const char *argv0){
@@ -11,5 +12,23 @@ int main(int argc,char **argv){
 		usage(*argv);
 		return EXIT_FAILURE;
 	}
+	if(libtorque_init()){
+		fprintf(stderr,"Error initializing libtorque.\n");
+		return EXIT_FAILURE;
+	}
+	if(libtorque_spawn()){
+		fprintf(stderr,"Error spawning libtorque.\n");
+		goto err;
+	}
+	if(libtorque_stop()){
+		fprintf(stderr,"Error shutting down libtorque.\n");
+		return EXIT_FAILURE;
+	}
 	return EXIT_SUCCESS;
+
+err:
+	if(libtorque_stop()){
+		fprintf(stderr,"Error shutting down libtorque.\n");
+	}
+	return EXIT_FAILURE;
 }
