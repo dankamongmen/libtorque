0539d2a0fd8cc43c648a59f1ce7a777443af3de3
Nick Black <dank@qemfd.net>
2009-11-12 01:14:29 -0500

    accept a -p option to torquessl, specifying service port

--- src/torquessl/torquessl.c
+++ src/torquessl/torquessl.c
@@ -40,6 +40,7 @@ static void
 usage(const char *argv0){
 	fprintf(stderr,"usage: %s [ options ]\n",argv0);
 	fprintf(stderr,"\t-h: print this message\n");
+	fprintf(stderr,"\t-p port: specify TCP port for service (default: %hu)\n",DEFAULT_PORT);
 	fprintf(stderr,"\t-C cafile: provide additional certificate authority\n");
 	fprintf(stderr,"\t-k keyfile: provide server key (requires -c)\n");
 	fprintf(stderr,"\t-c certfile: provide server cert (requires -k)\n");
@@ -47,23 +48,30 @@ usage(const char *argv0){
 
 static int
 parse_args(int argc,char **argv,const char **certfile,const char **keyfile,
-			const char **cafile){
-#define SET_ARG_ONCE(opt,arg) do{ if(!*(arg)){ *arg = optarg; }\
+			const char **cafile,uint16_t *port){
+#define SET_ARG_ONCE(opt,arg,val) do{ if(!*(arg)){ *arg = val; }\
 	else{ fprintf(stderr,"Provided '%c' twice\n",(opt)); goto err; }} while(0)
 
 	const char *argv0 = *argv;
 	int c;
 
-	while((c = getopt(argc,argv,"c:k:C:h")) > 0){
+	while((c = getopt(argc,argv,"c:k:C:p:h")) > 0){
 		switch(c){
+		case 'p': { int p = atoi(optarg);
+			if(p > 0xffff || p == 0){
+				goto err;
+			}
+			SET_ARG_ONCE('p',port,p);
+			break;
+		}
 		case 'c':
-			SET_ARG_ONCE('c',certfile);
+			SET_ARG_ONCE('c',certfile,optarg);
 			break;
 		case 'k':
-			SET_ARG_ONCE('k',keyfile);
+			SET_ARG_ONCE('k',keyfile,optarg);
 			break;
 		case 'C':
-			SET_ARG_ONCE('C',cafile);
+			SET_ARG_ONCE('C',cafile,optarg);
 			break;
 		case 'h':
 			usage(argv0);
@@ -92,12 +100,12 @@ int main(int argc,char **argv){
 	sigaddset(&termset,SIGINT);
 	sigaddset(&termset,SIGTERM);
 	memset(&sin,0,sizeof(sin));
-	if(parse_args(argc,argv,&certfile,&keyfile,&cafile)){
+	if(parse_args(argc,argv,&certfile,&keyfile,&cafile,&sin.sin_port)){
 		return EXIT_FAILURE;
 	}
 	sin.sin_family = AF_INET;
-	sin.sin_port = htons(DEFAULT_PORT); // FIXME ugh
 	sin.sin_addr.s_addr = htonl(INADDR_ANY);
+	sin.sin_port = htons(sin.sin_port ? sin.sin_port : DEFAULT_PORT);
 	if(pthread_sigmask(SIG_SETMASK,&termset,NULL)){
 		fprintf(stderr,"Couldn't set signal mask\n");
 		return EXIT_FAILURE;
