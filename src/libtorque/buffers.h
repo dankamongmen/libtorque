ab5d90fa6ef98a7f718c3c5e9ce2331cf0d72a83
Nick Black <dank@qemfd.net>
2009-11-15 09:46:37 -0500

    missed this

---
+++ src/libtorque/buffers.h
@@ -0,0 +1,41 @@
+#ifndef LIBTORQUE_BUFFERS
+#define LIBTORQUE_BUFFERS
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <libtorque/internal.h>
+
+// The simplest receive buffer.
+static inline char *
+rxbuffer_buf(libtorque_rxbuf *rxb){
+	return rxb->buffer;
+}
+
+static inline size_t
+rxbuffer_avail(const libtorque_rxbuf *rxb){
+	return rxb->bufleft;
+}
+
+#define RXBUFSIZE (16 * 1024)
+
+static inline int
+initialize_rxbuffer(libtorque_rxbuf *rxb){
+	if( (rxb->buffer = malloc(RXBUFSIZE)) ){
+		rxb->bufleft = RXBUFSIZE;
+		return 0;
+	}
+	return -1;
+}
+
+static inline void
+free_rxbuffer(libtorque_rxbuf *rxb){
+	free(rxb->buffer);
+}
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
