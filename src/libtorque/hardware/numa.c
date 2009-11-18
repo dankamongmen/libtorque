6b31eda8bf3f6438598d86f20b7e4e33fbc5e686
Nick Black <dank@qemfd.net>
2009-11-18 16:26:39 -0500

    strip numa diagnostics

--- src/libtorque/hardware/numa.c
+++ src/libtorque/hardware/numa.c
@@ -5,12 +5,10 @@
 #include <stdio.h>
 // LibNUMA looks like the only real candidate for NUMA discovery (linux only)
 int detect_numa(struct libtorque_ctx *ctx __attribute__ ((unused))){
-	printf("check for numa\n");
 	if(numa_available()){
-		printf("nope\n");
 		return 0;
 	}
-	printf("ayup\n");
+	// FIXME handle numa
 	return 0;
 }
 
