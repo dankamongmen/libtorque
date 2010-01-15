cd8694075f89c6d0f27512719269a79153096d05
Nick Black <dank@qemfd.net>
2010-01-14 20:54:53 -0500

    factor out makesigmask()

--- src/libtorque/libtorque.c
+++ src/libtorque/libtorque.c
@@ -116,6 +116,14 @@ libtorque_init_sigmasked(libtorque_err *e){
 	return ctx;
 }
 
+static inline int
+makesigmask(sigset_t *s){
+	if(sigfillset(s)){
+		return -1;
+	}
+	return 0;
+}
+
 libtorque_ctx *libtorque_init(libtorque_err *e){
 	struct sigaction oldact;
 	libtorque_ctx *ret;
@@ -138,7 +146,7 @@ libtorque_ctx *libtorque_init(libtorque_err *e){
 	// libtorque threads mask all signals, since they're either using
 	// EVFILT_SIGNAL/signalfd or epoll_pwait(), and we don't want to have
 	// other signals delivered (save synchronous ones, FIXME see bug 113).
-	if(sigfillset(&add) || pthread_sigmask(SIG_BLOCK,&add,&old)){
+	if(makesigmask(&add) || pthread_sigmask(SIG_BLOCK,&add,&old)){
 		*e = LIBTORQUE_ERR_ASSERT;
 		return NULL;
 	}
