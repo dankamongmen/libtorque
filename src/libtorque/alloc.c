8f4633c0ff49392e84826d2a55131bf7a30f4ca9
Nick Black <dank@qemfd.net>
2010-01-15 01:47:50 -0500

    introduce basic get_pages(), mod_pages()

--- src/libtorque/alloc.c
+++ src/libtorque/alloc.c
@@ -1,1 +1,13 @@
+#include <stdio.h>
+#include <sys/mman.h>
 #include <libtorque/alloc.h>
+
+void *get_pages(size_t s,int prot){
+	const int flags = MAP_SHARED | MAP_ANONYMOUS;
+
+	return mmap(NULL,s,prot,flags,-1,0);
+}
+
+void *mod_pages(void *map,size_t olds,size_t news,int prot){
+	return mremap(map,olds,news,prot);
+}
--- src/libtorque/alloc.h
+++ src/libtorque/alloc.h
@@ -5,6 +5,15 @@
 extern "C" {
 #endif
 
+void *get_pages(size_t,int)
+	__attribute__ ((warn_unused_result))
+	__attribute__ ((malloc));
+
+// __attribute__ ((malloc)) cannot be used with mod_pages (if the VMA is not
+// moved, the return value will alias the input pointer).
+void *mod_pages(void *,size_t,size_t,int)
+	__attribute__ ((warn_unused_result));
+
 #ifdef __cplusplus
 }
 #endif
