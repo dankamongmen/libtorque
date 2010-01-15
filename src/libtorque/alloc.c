e9ba4a1928a6ba860bcf7b5660222d27ae12fa71
Nick Black <dank@qemfd.net>
2010-01-15 02:05:56 -0500

    play around with a get_big_page()

--- src/libtorque/alloc.c
+++ src/libtorque/alloc.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <unistd.h>
 #include <sys/mman.h>
 #include <libtorque/alloc.h>
 
@@ -6,6 +7,11 @@ void *get_pages(size_t s,int prot){
 	const int flags = MAP_SHARED | MAP_ANONYMOUS;
 
 	return mmap(NULL,s,prot,flags,-1,0);
+}
+
+void *get_big_page(size_t *s){
+	*s = getpagesize(); // FIXME
+	return get_pages(*s,PROT_READ|PROT_WRITE);
 }
 
 void *mod_pages(void *map,size_t olds,size_t news,int prot){
--- src/libtorque/alloc.h
+++ src/libtorque/alloc.h
@@ -9,9 +9,20 @@ void *get_pages(size_t,int)
 	__attribute__ ((warn_unused_result))
 	__attribute__ ((malloc));
 
-// __attribute__ ((malloc)) cannot be used with mod_pages (if the VMA is not
-// moved, the return value will alias the input pointer).
+// Get a VMA as large as the largest (*architectural*, not necessarily
+// *supported*) TLB. The presumption is that this is a nice, natural size for
+// "something which might get big, but we definitely need a lot of them",
+// primarily per-connection buffers. Also, such an allocation will (hopefully)
+// trigger large TLB support in the OS immediately. The return is suitable for
+// use with mod_pages(). Returns the actual allocation size in the parameter.
+void *get_big_page(size_t *)
+	__attribute__ ((warn_unused_result))
+	__attribute__ ((nonnull(1)))
+	__attribute__ ((malloc));
+
 void *mod_pages(void *,size_t,size_t,int)
+	// __attribute__ ((malloc)) cannot be used with mod_pages (if the VMA is
+	// not moved, the return value will alias the input pointer).
 	__attribute__ ((warn_unused_result));
 
 #ifdef __cplusplus
